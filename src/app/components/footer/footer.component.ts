05b10998acca0c7497cbc6cfa6f2cce7e4d287d7
Adan Sandoval <adansinacento@gmail.com>
2023-12-01 12:43:18 -0600

    Revert "feat(footer.component): display the version at the footer"
    This reverts commit e23633eb75be06e5cb30e713f2ed98c0cd7c892c.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,17 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { AppVersionService } from 'src/app/services/app-version.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
-  version = '0.0.0';
-
-  constructor(private _versionService: AppVersionService) {}
-
-  ngOnInit(): void {
-    this.version = this._versionService.version;
-  }
-}
+export class FooterComponent {}
